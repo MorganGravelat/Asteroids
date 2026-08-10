c5d159f4dd5c8b10ba5cfa63816db293c590648b
MorganGravelat <mgravelat@gmail.com>
2026-08-10 16:17:52 -0400

    Created Player Object. Added player movement functionality. Updated players and objects into Groups

--- main.py
+++ main.py
@@ -1,21 +1,38 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 
-def main():
+def main() -> None:
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver} \n Screen width: {SCREEN_WIDTH} \n Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0.0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
 
     while True:
         log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill("black")
+        updatable.update(dt)
+        for drw in drawable:
+            drw.draw(screen)
         pygame.display.flip()
 
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
